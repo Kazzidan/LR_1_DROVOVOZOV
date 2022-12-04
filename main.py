7bc0c51ddf289d485738df0606af17bda14759eb
byste <byster1234@mail.ru>
2022-12-04 21:14:25 +0300

    created project

---
+++ main.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+s = Service('D:/download/chomedriver.exe')
+driver = webdriver.Chrome(service=s)
+driver.get('https://adminqa.neapro.site/login')
+driver.set_window_size(1024, 600)
+driver.minimize_window()
+driver.find_element(By.ID, 'admin_email').click()
+driver.find_element(By.ID, 'admin_email').send_keys('moderat@neapro.ru')
+driver.find_element(By.ID, 'admin_password').click()
+driver.find_element(By.ID, 'admin_password').send_keys('Aa123456')
+driver.find_element(By.NAME, 'commit').click()
