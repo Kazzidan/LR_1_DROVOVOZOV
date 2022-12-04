c65b55f6428b0918008195ea70325eb40c27b54f
byste <byster1234@mail.ru>
2022-12-04 22:17:27 +0300

    test 1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
-s = Service('D:/download/chomedriver.exe')
+s = Service("G:/driver/chromedriver.exe")
 driver = webdriver.Chrome(service=s)
 driver.get('https://adminqa.neapro.site/login')
 driver.set_window_size(1024, 600)
@@ -12,3 +12,4 @@ driver.find_element(By.ID, 'admin_email').send_keys('moderat@neapro.ru')
 driver.find_element(By.ID, 'admin_password').click()
 driver.find_element(By.ID, 'admin_password').send_keys('Aa123456')
 driver.find_element(By.NAME, 'commit').click()
+
